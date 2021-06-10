5d61219591fed7f92f9e94e0fd66b60986d69ee9
LG West <larsgwest@gmail.com>
2021-06-10 21:10:26 +0200

    5.3.3 Visitors for expressions

--- java/com/tool/GenerateAST.java
+++ java/com/tool/GenerateAST.java
@@ -21,6 +21,7 @@ public class GenerateAST {
                 "Unary    : Token operator, Expr right"
         ));
     }
+
     private static void defineAst(
             String outputDir, String baseName, List<String> types)
             throws IOException {
@@ -33,15 +34,36 @@ public class GenerateAST {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
     }
+
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
+    }
+
     private static void defineType(
             PrintWriter writer, String baseName,
             String className, String fieldList) {
@@ -60,12 +82,20 @@ public class GenerateAST {
 
         writer.println("    }");
 
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println("    }");
+
         // Fields.
         writer.println();
         for (String field : fields) {
             writer.println("    final " + field + ";");
         }
-
+        
         writer.println("  }");
     }
 }
